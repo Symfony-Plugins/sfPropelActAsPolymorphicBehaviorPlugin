Symfony-Plugins/sfPropelActAsPolymorphicBehaviorPlugin

commit fa50224471f9b2bc18ad18a99cee815490969638

sfPropelActAsPolymorphicBehaviorPlugin: Added automatic mixin of custom methods when using symfony >= 1.1
git-svn-id: http://svn.symfony-project.com/plugins/sfPropelActAsPolymorphicBehaviorPlugin/trunk@14233 ee427ae8-e902-0410-961c-c3ed070cd9f9

lib/sfPropelActAsPolymorphicBehavior.class.php
@@ -52,6 +52,52 @@ class sfPropelActAsPolymorphicBehavior
   }
   
   /**
+   * Listens for the 'context.load_factories' event in symfony >= 1.1 to mixin custom methods.
+   * 
+   * @param sfEvent $event
+   */
+  static public function listenForLoadFactories(sfEvent $event)
+  {
+    $context       = $event->getSubject();
+    $configuration = $context->getConfiguration();
+
+    $cache = new sfFileCache(array(
+      'cache_dir' => sfConfig::get('sf_config_cache_dir').'/sfPropelActAsPolymorphic',
+    ));
+
+    if (is_null($classes = $cache->get('classes')))
+    {
+      $classes = array();
+
+      $finder = sfFinder::type('file')->name('*MapBuilder.php');
+      foreach ($finder->in($configuration->getModelDirs()) as $builder)
+      {
+        $class = basename($builder, 'MapBuilder.php');
+        $fmt = 'propel_behavior_sfPropelActAsPolymorphic_'.$class.'_has_%s';
+        if (
+          class_exists($class)
+          &&
+          (sfConfig::has(sprintf($fmt, 'one')) || sfConfig::has(sprintf($fmt, 'many')))
+        )
+        {
+          $classes[] = $class;
+        }
+      }
+
+      $cache->set('classes', serialize($classes));
+    }
+    else
+    {
+      $classes = unserialize($classes);
+    }
+
+    foreach ($classes as $class)
+    {
+      self::mixinCustomMethods($class);
+    }
+  }
+  
+  /**
    * Process has_one references.
    * 
    * If a modified foreign object exist in the parameter holder for any of

config/config.php
@@ -21,3 +21,8 @@ sfPropelBehavior::registerMethods('sfPropelActAsPolymorphic', array(
   array('sfPropelActAsPolymorphicBehavior', 'addPolymorphicHasManyReference'),
   array('sfPropelActAsPolymorphicBehavior', 'countPolymorphicHasManyReferences'),
 ));
+
+if ($this instanceof sfProjectConfiguration)
+{
+  $this->dispatcher->connect('context.load_factories', array('sfPropelActAsPolymorphicBehavior', 'listenForLoadFactories'));
+}

commit 4f4ee4681e7bd526516a6d1f8a82025c9843c663

sfPropelActAsPolymorphicBehaviorPlugin: fixed regression introduced in r14233
git-svn-id: http://svn.symfony-project.com/plugins/sfPropelActAsPolymorphicBehaviorPlugin/trunk@14282 ee427ae8-e902-0410-961c-c3ed070cd9f9

lib/sfPropelActAsPolymorphicBehavior.class.php
@@ -39,13 +39,20 @@ class sfPropelActAsPolymorphicBehavior
         {
           $method = $prefix.$camelCase;
           
-          if (!isset(sfPropelActAsPolymorphicBehavior::$customMethods[$omClass]))
+          try
           {
-            sfPropelActAsPolymorphicBehavior::$customMethods[$omClass] = array();
+            sfMixer::register('Base'.$omClass, array(new sfPropelActAsPolymorphicBehavior, $method));
+
+            if (!isset(sfPropelActAsPolymorphicBehavior::$customMethods[$omClass]))
+            {
+              sfPropelActAsPolymorphicBehavior::$customMethods[$omClass] = array();
+            }
+            sfPropelActAsPolymorphicBehavior::$customMethods[$omClass][$method] = array($type, $prefix, $name);
           }
-          sfPropelActAsPolymorphicBehavior::$customMethods[$omClass][$method] = array($type, $prefix, $name);
-          
-          sfMixer::register('Base'.$omClass, array(new sfPropelActAsPolymorphicBehavior, $method));
+          catch (Exception $e)
+          {
+            // methods have most likely already been mixed in
+          }
         }
       }
     }